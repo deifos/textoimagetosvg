4926858ade1304038b3239e299271b29bc2ac7d1
Vlad <vlad.palacio@gmail.com>
2025-07-21 02:08:59 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fal.media',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'v3.fal.media',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
